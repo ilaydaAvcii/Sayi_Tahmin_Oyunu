bf2486a4b379139fa637b0bdafcfa30986bffcf5
iavci <iavci@LAPTOP-4674SJUE>
2025-03-16 14:01:25 +0300

    Add project files.

---
+++ sayitahminoyunu/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int hedefSayi = random.Next(1, 101);  // 1 ile 100 arasında rastgele sayı
+        int tahmin = 0;
+        int tahminSayisi = 0;
+
+        Console.WriteLine("1 ile 100 arasında bir sayı tuttum! Tahmin etmeye çalış.");
+
+        while (tahmin != hedefSayi)
+        {
+            Console.Write("Tahmininizi girin: ");
+            tahmin = int.Parse(Console.ReadLine());  
+            tahminSayisi++;
+
+            if (tahmin < hedefSayi)
+            {
+                Console.WriteLine("Tahmininiz küçük. Daha büyük bir sayı deneyin.");
+            }
+            else if (tahmin > hedefSayi)
+            {
+                Console.WriteLine("Tahmininiz büyük. Daha küçük bir sayı deneyin.");
+            }
+        }
+        Console.WriteLine($"Tebrikler! Doğru tahmin ettiniz. {tahminSayisi} tahminde bulundunuz.");
+    }
+}
